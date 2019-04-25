90e3ad57aa20ed62e72bb9573174b1e1e917d6f4
qchj20131252 <867916544@qq.com>
2019-04-25 23:25:03 +0800

    修复

--- mnist_classification/__init__.py
+++ mnist_classification/__init__.py
@@ -30,40 +30,53 @@ def convolutional_neural_network(input):
     return fc
 
 def main():
+    #获取mnist数据
+    batch_size = 128
+    train_reader = paddle.batch(mnist.train(),batch_size=batch_size)
+    test_reader = paddle.batch(mnist.test(),batch_size=batch_size)
+
     #定义输入层
     image = fluid.layers.data(name="image", shape=[1,28,28], dtype="float32")
     label = fluid.layers.data(name="label", shape=[1], dtype="int64")
 
     #获取分类器
-    # model = multilayers_perceptron(image)
-    model = convolutional_neural_network(image)
+    model = multilayers_perceptron(image)
+    # model = convolutional_neural_network(image)
+
+    main_program = fluid.default_main_program()#获取默认/全局主函数
+    startup_program = fluid.default_startup_program()#获取默认/全局启动程序
 
     #获取损失函数和准确率函数
     cost = fluid.layers.cross_entropy(input=model,label=label)
     avg_cost = fluid.layers.mean(cost)
     acc = fluid.layers.accuracy(input=model,label=label)
 
-    test_program = fluid.default_main_program().clone(for_test=True)
+    #获取测试程序
+    test_program = main_program.clone(for_test=True)
 
+    #定义优化方法，交叉熵损失函数常用于分类任务
     optimizer = fluid.optimizer.AdamOptimizer(learning_rate=0.001)
     optimizer.minimize(avg_cost)
 
-    train_reader = paddle.batch(mnist.train(),batch_size=128)
-    test_reader = paddle.batch(mnist.test(),batch_size=128)
-
     #定义执行器
-    use_cuda = False
+    use_cuda = True
     place = fluid.CUDAPlace(0) if use_cuda else fluid.CPUPlace()
     exe = fluid.Executor(place)
 
-    #进行参数初始化
-    exe.run(fluid.default_startup_program())
+    # 指定保存参数的目录
+    params_dirname = "./model/mnist_classification.inference.model"
+    num_epochs = 10
 
     #定义输入数据维度
     feeder = fluid.DataFeeder(place=place, feed_list=[image,label])
 
+    #进行参数初始化
+    exe.run(startup_program)
+
+    exe_test = fluid.Executor(place)
+
     #开始训练和进行测试
-    for pass_id in range(5):
+    for pass_id in range(num_epochs):
         #进行训练
         for batch_id, data in enumerate(train_reader()):
             train_cost, train_acc = exe.run(program=fluid.default_main_program(), feed=feeder.feed(data), fetch_list=[avg_cost,acc])
@@ -71,17 +84,18 @@ def main():
             if batch_id % 100 == 0:
                 print('Pass:%d, Batch:%d, Cost:%0.5f, Accuracy:%0.5f' % (pass_id, batch_id, train_cost[0], train_acc[0]))
 
+
         #进行测试
         test_accs = []
         test_costs = []
         for batch_id, data in enumerate(test_reader()):
-            test_cost, test_acc = exe.run(program=fluid.default_main_program(), feed=feeder.feed(data), fetch_list=[avg_cost,acc])
-            test_accs.append(test_accs)
-            test_costs.append(test_cost)
+            test_cost, test_acc = exe_test.run(program=test_program, feed=feeder.feed(data), fetch_list=[avg_cost,acc])
+            test_accs.append(test_acc[0])
+            test_costs.append(test_cost[0])
 
         #求测试结果的平均值
-        test_cost = sum(test_costs) / len(test_costs)
-        test_acc = sum(test_accs) / len(test_accs)
+        test_cost = np.sum(test_costs) / len(test_costs)
+        test_acc = np.sum(test_accs) / len(test_accs)
 
         print('Test:%d, Cost:%0.5f, Accuracy:%0.5f' % (pass_id, test_cost, test_acc))
 
